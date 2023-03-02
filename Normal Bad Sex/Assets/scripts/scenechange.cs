b6012fdc43b14f6b68fc13594a05e2ca378f4f7d
Swasams <92123037+Swasams@users.noreply.github.com>
2023-03-01 21:15:57 -0500

    title scene

---
+++ Normal Bad Sex/Assets/scripts/scenechange.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechange : MonoBehaviour
+{
+   
+    
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Cenza Original");
+    }
+
+}
