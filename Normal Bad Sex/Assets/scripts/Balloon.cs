df115741956f6028c4cf4152daccf930d25cb420
Swasams <92123037+Swasams@users.noreply.github.com>
2023-03-02 11:11:52 -0500

    ch 16-20

--- Normal Bad Sex/Assets/scripts/Balloon.cs
+++ Normal Bad Sex/Assets/scripts/Balloon.cs
@@ -24,7 +24,7 @@ public class Balloon : MonoBehaviour
 
             if (Input.GetMouseButtonDown(0))
             {
-                balloon.Play("blueB");
+                balloon.Play("Balloon");
             }
 
         }
