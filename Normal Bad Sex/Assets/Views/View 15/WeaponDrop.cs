d647be6efb60fcb5497e36db8c53efcb6f16b4cc
nyusha-jpg <ai24@nyu.edu>
2023-05-04 01:03:47 -0400

    V15

--- Normal Bad Sex/Assets/Views/View 15/WeaponDrop.cs
+++ Normal Bad Sex/Assets/Views/View 15/WeaponDrop.cs
@@ -1,29 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class WeaponDrop : MonoBehaviour
 {
-    //public GameObject thisWeapon;
-    public float moveSpeed = 1.0f;
+    public GameObject thisWeapon;
+    public float moveSpeed;
     public Vector2 targetPosition;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //DOTween.Init();
-
+       
     }
 
     // Update is called once per frame
     void Update()
     {
 
-       transform.position = Vector3.Lerp(this.transform.position, targetPosition, Time.time);
+       //transform.position = Vector3.Lerp(this.transform.position, targetPosition, Time.time);
 
         //new Vector2(moveSpeed * Time.deltaTime, targetPosition);
+    }
+
+    public void DropIt()
+    {
+        /*
+        this.gameObject.GetComponent<Animator>().speed = 0;
+
+        LeanTween.moveY(thisWeapon, transform.position.y - 00000f, moveSpeed);
+        
+        LeanTween.scale(thisWeapon, new Vector2(0,0), moveSpeed);
+        Debug.Log("heyo drop it script");
+        */
     }
 
 }
