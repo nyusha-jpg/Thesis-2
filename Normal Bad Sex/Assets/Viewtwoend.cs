2a595c56ab7261b5bce0cc8c437233ed51a6b5ca
Swasams <92123037+Swasams@users.noreply.github.com>
2023-05-07 20:04:18 -0400

    till view 15

--- Normal Bad Sex/Assets/Viewtwoend.cs
+++ Normal Bad Sex/Assets/Viewtwoend.cs
@@ -12,7 +12,7 @@ public class Viewtwoend : MonoBehaviour
     public Animator four;
     public Animator five;
     Animator anim;
-
+    private bool animationPlayed = false;
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
@@ -21,14 +21,15 @@ public class Viewtwoend : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (one.GetCurrentAnimatorStateInfo(0).IsName("GlassFill") && 
+        if (!animationPlayed && one.GetCurrentAnimatorStateInfo(0).IsName("GlassFill") && 
             two.GetCurrentAnimatorStateInfo(0).IsName("GlassFill") && 
             three.GetCurrentAnimatorStateInfo(0).IsName("GlassFill") &&
             four.GetCurrentAnimatorStateInfo(0).IsName("GlassFill") && 
             five.GetCurrentAnimatorStateInfo(0).IsName("GlassFill"))
         {
             anim.Play("eomg anim");
-            Debug.Log("nicejob");
+            animationPlayed = true;
+            //Debug.Log("nicejob");
         }
     }
 }
